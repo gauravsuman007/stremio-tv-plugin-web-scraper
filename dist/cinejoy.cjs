29f3155e3eaccf88205c03159de1e009aa4d8e21
Gaurav Suman <gauravsuman007@gmail.com>
2026-09-25 22:48:17 +0200

    Bump to 1.0.2 to force a resync with the fixed importer

--- dist/cinejoy.cjs
+++ dist/cinejoy.cjs
@@ -194,7 +194,7 @@ async function search(query, ctx) {
 var cinejoyScraper = {
   id: "cinejoy",
   name: "CineJoy",
-  version: "1.0.1",
+  version: "1.0.2",
   search
 };
 var scraper_default = cinejoyScraper;
