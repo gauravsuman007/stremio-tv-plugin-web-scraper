2cce98f6d0eb3e38aeb2bb9f5a16d580366b1822
Gaurav Suman <gauravsuman007@gmail.com>
2026-09-25 23:30:11 +0200

    Bump to 1.0.8 for the post-restart resync

--- dist/cinejoy.cjs
+++ dist/cinejoy.cjs
@@ -194,7 +194,7 @@ async function search(query, ctx) {
 var cinejoyScraper = {
   id: "cinejoy",
   name: "CineJoy",
-  version: "1.0.7",
+  version: "1.0.8",
   search
 };
 var scraper_default = cinejoyScraper;
